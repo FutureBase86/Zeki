28ccc448392e914e4fd73adbf1ecf050b27c8157
AlpayCa <alpay41@gmail.com>
2023-02-22 20:36:47 +0300

    DegiskenlerAtandı

--- Program.cs
+++ Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Hazırlayan AlpayCa");
+            Console.WriteLine("Şifre Kontrol Programı... ");
+
+            int reelPin = 1144, reelPuk = 1122334455, pin, yeniPin, puk,
+            pinBloke = 3, pukBloke = 10, onayKodu = 0,
+            musteriTemsilcisiOnayKodu = 0123456789;
+
+            Console.Write("Lütfen Pin Kodunu Girin : ");
+            pin = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
