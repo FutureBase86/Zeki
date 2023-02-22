c40e7b590905fde7a05451fc4e9aa3734c2a34d2
AlpayCa <alpay41@gmail.com>
2023-02-22 20:48:12 +0300

    AcılıyorEkendi

--- Program.cs
+++ Program.cs
@@ -31,17 +31,31 @@
                         {
                             Console.Write("Lütfen Puk Kodunu Girin : ");
                             puk = Convert.ToInt32(Console.ReadLine());
-                        }
-                        if (j >= 10)
-                        {
-                            Console.WriteLine("Puk Kodunuz Bloke OLdu...\nLütfen Müşteri Hizmetleri İle İletişime Geçiniz.");
-                            Console.Write("Müşteri Temsilcisi Onay Kodu : ");
-                            onayKodu = Convert.ToInt32(Console.ReadLine());
-                            if (onayKodu != musteriTemsilcisiOnayKodu)
+
+                            if (j >= 10)
                             {
-                                Console.WriteLine("Sistem Kilitlendi...");
+                                Console.WriteLine("Puk Kodunuz Bloke OLdu...\nLütfen Müşteri Hizmetleri İle İletişime Geçiniz.");
+                                Console.Write("Müşteri Temsilcisi Onay Kodu : ");
+                                onayKodu = Convert.ToInt32(Console.ReadLine());
+                                if (onayKodu != musteriTemsilcisiOnayKodu)
+                                {
+                                    Console.WriteLine("Sistem Kilitlendi...");
+                                    break;
+                                }
+                            }
+                            if (puk == reelPuk || onayKodu == musteriTemsilcisiOnayKodu)
+                            {
+                                reelPin = 1144;
+                                Console.Write("Yeni pin Giriniz : ");
+                                reelPin = Convert.ToInt32(Console.ReadLine());
                                 break;
                             }
                         }
                     }
+                }
+                break;
+            }
+            Console.WriteLine("Açılıyor...");
+        }
+    }
 }
